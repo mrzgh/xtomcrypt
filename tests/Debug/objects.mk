f8febb789c13d0a4a6b0c6e14693c156fd63467a
mrzgh <k3kcokl4t@gmail.com>
2018-01-18 14:43:01 +0800

    adding tests directory

---
+++ tests/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := C:/cygwin64/home/reza.zaba/xtomcrypt/trunk/libxtomcrypt.a
+
+LIBS :=
+
---
+++ tests/Debug/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../tests.c 
+
+OBJS += \
+./tests.o 
+
+C_DEPS += \
+./tests.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cygwin C Compiler'
+	gcc -I"C:\cygwin64\home\reza.zaba\xtomcrypt\trunk\src\headers" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
